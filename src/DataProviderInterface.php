2292f35cf703b0ae36f6f10e2fc6ddb059720d2c
John Pancoast <johnpancoaster@gmail.com>
2016-11-25 13:54:16 -0800

    Add format methods to data provider

--- src/DataProviderInterface.php
+++ src/DataProviderInterface.php
@@ -8,12 +8,23 @@
 namespace Pancoast\DataProcessor;
 
 /**
- * A data provider... provides data.
- *
- * It's an iterator at its most basic
+ * An iterator that provides data and specifies its current format
  *
  * @author John Pancoast <johnpancoaster@gmail.com>
  */
 interface DataProviderInterface extends \Iterator
 {
+    /**
+     * Set format of data
+     *
+     * @param string $format
+     */
+    public function setFormat($format);
+
+    /**
+     * Get format of data
+     *
+     * @return string
+     */
+    public function getFormat();
 }
